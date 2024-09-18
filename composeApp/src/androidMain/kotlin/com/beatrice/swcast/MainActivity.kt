6a2949a2f463792a112dd7ab0d0af4639882f09b
BKinya <Gitongakinya1@gmail.com>
2024-09-18 19:06:31 +0300

    enable edge to edge

--- composeApp/src/androidMain/kotlin/com/beatrice/swcast/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/beatrice/swcast/MainActivity.kt
@@ -3,6 +3,7 @@ package com.beatrice.swcast
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -13,6 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        enableEdgeToEdge()
 
         setContent {
             App()
