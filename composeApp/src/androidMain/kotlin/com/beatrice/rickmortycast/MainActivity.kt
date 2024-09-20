37bbe3da0c81c2d18132cdcd31553ad819526dd4
BKinya <Gitongakinya1@gmail.com>
2024-09-20 11:44:40 +0300

    polish ui

--- composeApp/src/androidMain/kotlin/com/beatrice/rickmortycast/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/beatrice/rickmortycast/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.beatrice.rickmortycast.presentation.App
 
-
+val darkGrey = Color(0xFF222222)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +23,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            App()
+            Box(modifier = Modifier.background(color = darkGrey)) {
+                App(
+                    modifier = Modifier.padding(top = 40.dp)
+                )
+            }
         }
     }
 }
